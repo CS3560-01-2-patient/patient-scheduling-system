342d5d6a8ec433c366c788454497df9eece20d6f
Christian Grewal <58606181+cgrew77@users.noreply.github.com>
2023-03-17 17:46:47 -0700

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,3 +1,58 @@
 public class Patient {
     
+    private string name;
+    private int idNumber;
+    private int phoneNumber;
+    private string emailAddress;
+    private int socialSecurityNumber;
+    private int healthInsuranceNumber
+    
+    public string getPatientName() {
+        return name;
+    }
+    
+    public void setPatientName() {
+        name = newValue;
+    } 
+    
+    public string getIdNumber() {
+        return idNumber;
+    }
+    
+    public void setIdNumber() {
+        idNumber = newValue;
+    }   
+    
+    public string getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber() {
+        phoneNumber = newValue;
+    }    
+    
+    public string getEmailAddress() {
+        return emailAddress;
+    }
+    
+    public void setEmailAddress() {
+        emailAddress = newValue;
+    }    
+    
+    public string getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+    
+    public void setSocialSecurityNumber() {
+        socialSecurityNumber = newValue;
+    }    
+    
+    public string getHealthInsuranceNumber() {
+        return healthInsuranceNumber;
+    }
+    
+    public void setHealthInsuranceNumber() {
+        healthInsuranceNumber = newValue;
+    }
+    
 }
