d0790e57b6474ae11f955b965640abb6509d5f11
Brandon Diep <brandondiep2711@gmail.com>
2023-03-17 18:55:26 -0700

    added getter and setter methods

--- Appointment.java
+++ Appointment.java
@@ -1,3 +1,81 @@
 public class Appointment {
-    
+
+    private String date;
+    private String time;
+    private String treatment;
+    private String status;
+
+     /**
+	 * getDate method
+	 * @return The appointment date
+	 */
+    public String getDate()
+    {
+        return date;
+    }
+
+    /**
+	 * setDate method
+	 * @param date The date of the appointment 
+	 */
+    public String setDate(String appointmentDate)
+    {
+        date = appointmentDate;
+    }
+
+
+    /**
+	 * getTime method
+	 * @return The appointment time
+	 */
+    public String getTime()
+    {
+        return time;
+    }
+
+    /**
+	 * setTime method
+	 * @param time The time of the appointment 
+	 */
+    public String setTime(String appointmentTime)
+    {
+        time = appointmentTime;
+    }
+
+    /**
+	 * getTreatment method
+	 * @return The treatment diagnosis from the appointment
+	 */
+    public String getTreatment()
+    {
+        return treatment;
+    }
+
+    /**
+	 * setTreatment method
+	 * @param treat The treatment given from appointment
+	 */
+    public String setTreatment(String treat)
+    {
+        treatment = treat;
+    }
+
+    /**
+	 * getStatus method
+	 * @return The appointment status
+	 */
+    public String getStatus()
+    {
+        return status;
+    }
+
+    /**
+	 * setStatus method
+	 * @param appointmentStatus the status of the appointment
+	 */
+    public String setTime(String appointmentStatus)
+    {
+        status = appointmentStatus;
+    }
+
 }
